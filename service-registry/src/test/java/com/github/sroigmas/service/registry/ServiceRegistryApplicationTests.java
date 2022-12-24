1148f2d064c15d75e7ffd5348bd6bd6e94cd23fe
Salva Roig <salroima@gmail.com>
2022-12-24 19:51:41 +0100

    Adding Eureka Server

---
+++ service-registry/src/test/java/com/github/sroigmas/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sroigmas.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
